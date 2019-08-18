0d4d4a983d5616c235a33d36f7d911662c5c6472
snwu <snwu.cupdata.com>
2019-08-18 22:11:32 +0800

    eureka

---
+++ src/main/java/cn/lqdev/learning/springcloud/eureka/server/EureakServiceApplication.java
@@ -0,0 +1,21 @@
+package cn.lqdev.learning.springcloud.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Eureka服务端
+ * @author oKong
+ *
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EureakServiceApplication {
+	
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(EureakServiceApplication.class, args);
+	}
+}
